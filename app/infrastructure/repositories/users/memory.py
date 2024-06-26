a82f453dc5249392a4d97aba33cc3a04f6ecb334
Alexey Volkov <vollkovalex1991@mail.ru>
2024-06-26 11:27:05 +0400

    Enhance memory repository implementation

--- app/infrastructure/repositories/users/memory.py
+++ app/infrastructure/repositories/users/memory.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Iterable
 
-from domain.entities.users import UserEntity
+from infrastructure.models.users import UserModel
 from infrastructure.repositories.users.base import IUserRepository
 from infrastructure.repositories.users.filters.users import GetUsersFilters
 
@@ -10,30 +10,33 @@ from infrastructure.repositories.users.filters.users import GetUsersFilters
 class InMemoryUserRepository(IUserRepository):
     _saved_users: list[UserEntity] = field(default_factory=list, kw_only=True)
 
-    async def add_user(self, user: UserEntity) -> None:
+    async def add_user(self, user: UserModel) -> None:
         self._saved_users.append(user)
 
-    async def get_by_oid(self, user_oid: str) -> UserEntity | None:
+    async def get_by_oid(self, user_oid: str) -> UserModel | None:
         for user in self._saved_users:
             if user.oid == user_oid:
                 return user
-        return None
+
+    async def get_by_username(self, user_oid: str) -> UserModel | None:
+        for user in self._saved_users:
+            if user.username == username:
+                return user
 
     async def get_all(
         self, filters: GetUsersFilters
-    ) -> tuple[Iterable[UserEntity], int]:
+    ) -> tuple[Iterable[UserModel], int]:
         total_count = len(self._saved_users)
         limited_users = self._saved_users[
             filters.offset : filters.offset + filters.limit
         ]
         return limited_users, total_count
 
-    async def delete(self, user_oid: str) -> UserEntity | None:
+    async def delete(self, user_oid: str) -> UserModel | None:
         for user in self._saved_users:
             if user.oid == user_oid:
                 self._saved_users.remove(user)
                 return user
-        return None
 
     async def check_user_exists_by_phone_and_username(
         self, phone: str, username: str
