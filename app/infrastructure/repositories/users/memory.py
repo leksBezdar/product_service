d9ad498cd09f7bc58a2a394f31550865f7727008
leks <vollkovalex1991@mail.ru>
2024-06-30 22:13:54 +0400

    Fix user memory repository

--- app/infrastructure/repositories/users/memory.py
+++ app/infrastructure/repositories/users/memory.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass, field
 from typing import Iterable
-
-from infrastructure.models.users import UserModel
+from domain.entities.users import UserEntity
+from domain.values.users import Username
 from infrastructure.repositories.users.base import IUserRepository
 from infrastructure.repositories.users.filters.users import GetUsersFilters
 
@@ -10,39 +10,47 @@ from infrastructure.repositories.users.filters.users import GetUsersFilters
 class InMemoryUserRepository(IUserRepository):
     _saved_users: list[UserEntity] = field(default_factory=list, kw_only=True)
 
-    async def add_user(self, user: UserModel) -> None:
+    async def add(self, user: UserEntity) -> None:
         self._saved_users.append(user)
 
-    async def get_by_oid(self, user_oid: str) -> UserModel | None:
+    async def get_by_oid(self, oid: str) -> UserEntity | None:
         for user in self._saved_users:
-            if user.oid == user_oid:
+            if user.oid == oid:
                 return user
 
-    async def get_by_username(self, user_oid: str) -> UserModel | None:
+    async def get_by_username(self, username: str) -> UserEntity | None:
         for user in self._saved_users:
             if user.username == username:
                 return user
 
+    async def get_existing_usernames(self) -> list[Username]:
+        return [user.username for user in self._saved_users]
+
+    async def check_user_exists_by_phone_and_username(
+        self, phone: str, username: str
+    ) -> bool:
+        return any(
+            user.phone == phone or user.username == username
+            for user in self._saved_users
+        )
+
     async def get_all(
         self, filters: GetUsersFilters
-    ) -> tuple[Iterable[UserModel], int]:
+    ) -> tuple[Iterable[UserEntity], int]:
         total_count = len(self._saved_users)
         limited_users = self._saved_users[
             filters.offset : filters.offset + filters.limit
         ]
         return limited_users, total_count
 
-    async def delete(self, user_oid: str) -> UserModel | None:
-        for user in self._saved_users:
-            if user.oid == user_oid:
-                self._saved_users.remove(user)
+    async def update(self, user: UserEntity) -> UserEntity:
+        for i, u in enumerate(self._saved_users):
+            if u.oid == user.oid:
+                self._saved_users[i] = user
                 return user
 
-    async def check_user_exists_by_phone_and_username(
-        self, phone: str, username: str
-    ) -> bool:
-        return any(
-            user.phone.as_generic_type() == phone
-            or user.username.as_generic_type() == username
-            for user in self._saved_users
-        )
+    async def delete(self, oid: str) -> UserEntity | None:
+        for user in self._saved_users:
+            if user.oid == oid:
+                self._saved_users.remove(user)
+                return user
